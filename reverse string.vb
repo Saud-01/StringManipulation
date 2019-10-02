c05f79aee3b543fc9241df3199d44461d70ebe42
Saud-01 <55662925+Saud-01@users.noreply.github.com>
2019-10-02 12:01:22 +0500

    Visual Basic
    Reverses the input string

---
+++ reverse string.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Str, letter, rStr As String
+        Dim counter As Integer
+
+            Str = ""
+            counter = 0
+            letter = ""
+            rStr = ""
+
+            Console.Write("Input your String: ")
+            Str = Console.ReadLine
+
+            For counter = Len(Str) To 1 Step -1
+                letter = Mid(Str, counter, 1)
+                rStr = rStr & letter
+            Next
+
+            Console.Write(rStr)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
