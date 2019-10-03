f96c65321f2c764142127697ea48b0f3a5b42202
Saud-01 <55662925+Saud-01@users.noreply.github.com>
2019-10-03 15:51:38 +0500

    Visual Basic

---
+++ Middle String.vb
@@ -0,0 +1,35 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Middle, str1, con, last2w As String
+        Dim sp As Integer
+
+        Do
+
+            Middle = " "
+            str1 = ""
+            con = ""
+            sp = 0
+            last2w = ""
+
+            Console.Write("Enter your string: ")
+            str1 = Console.ReadLine
+
+            sp = InStr(str1, " ")
+            last2w = Right(str1, Len(str1) - sp)
+
+            sp = InStr(last2w, " ")
+            Middle = Left(last2w, sp - 1)
+
+            Console.WriteLine("'" & Middle & "'" & " is the middle word")
+
+            Console.WriteLine(" ")
+            Console.Write("Do you want to continue ?: ")
+            con = Console.ReadLine
+            con = LCase(con)
+
+        Loop Until con = "no"
+
+    End Sub
+
+End Module
