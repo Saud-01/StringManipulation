a2c69c74530e054a3112e8d5b9d6460dc537afbd
Saud-01 <55662925+Saud-01@users.noreply.github.com>
2019-10-04 16:32:48 +0500

    Visual Basic

---
+++ Character Types.vb
@@ -0,0 +1,46 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str, con, letter, alph, num, sign As String
+        Dim count As Integer
+
+        Do
+
+            alph = ""
+            num = ""
+            sign = ""
+            str = ""
+            con = ""
+            letter = ""
+            count = 0
+
+            Console.Write("Input your string: ")
+            str = Console.ReadLine
+
+            For count = 1 To Len(str)
+                letter = Asc(Mid(str, count, 1))
+                If letter >= 65 And letter <= 90 Or letter >= 97 And letter <= 122 Then
+                    alph = alph & Chr(letter) & " "
+
+                ElseIf letter >= 48 And letter <= 57 Then
+                    num = num & Chr(letter) & " "
+
+                Else : sign = sign & Chr(letter) & " "
+                End If
+            Next
+
+            Console.WriteLine("Alphabets: " & alph)
+            Console.WriteLine("Numbers: " & num)
+            Console.WriteLine("Special Charactes: " & sign)
+
+            Console.WriteLine("  ")
+            Console.Write("Do you want to continue ?:")
+            con = Console.ReadLine
+
+            con = LCase(con)
+
+        Loop Until con = "no"
+
+    End Sub
+
+End Module
