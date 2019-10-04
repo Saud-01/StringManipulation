e2d9886605bd5ada9402f278e4031ce25b88a7a2
Saud-01 <55662925+Saud-01@users.noreply.github.com>
2019-10-04 16:46:04 +0500

    Visual Basic

---
+++ Truncating Character.vb
@@ -0,0 +1,45 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim NewString, con, Instring As String
+        Dim Index As Integer
+        Dim AfterSpace As Boolean
+        Dim NextChar As Char
+        Dim Space As String
+
+        Do
+            con = ""
+            Space = ""
+            AfterSpace = False
+            NewString = ""
+
+
+            Console.Write("Enter New String: ")
+            Instring = Console.ReadLine
+
+            Console.Write("Which character do you want to truncate: ")
+            Space = Console.ReadLine
+
+
+            For Index = 1 To Len(Instring)
+                NextChar = (Mid(Instring, Index, 1))
+
+                If NextChar <> Space Then
+                    NewString = NewString & NextChar
+                End If
+            Next
+            Console.WriteLine(NewString)
+
+            Console.WriteLine("  ")
+            Console.Write("Do you want to continue ?:")
+            con = Console.ReadLine
+
+            con = LCase(con)
+
+        Loop Until con = "no"
+
+
+    End Sub
+
+End Module
